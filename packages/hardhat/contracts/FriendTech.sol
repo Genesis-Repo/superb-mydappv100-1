55fad6deae3c4c09bbd08c69e9451144d7ba079c
mike-menyala <159994059+mike-menyala@users.noreply.github.com>
2024-03-26 16:08:07 +0800

    commit message for multiple files

---
+++ packages/hardhat/contracts/FriendTech.sol
@@ -0,0 +1,84 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract FriendTech is ERC20, AccessControl {
+    address public owner;
+
+    mapping(address => uint256) private sharePrice;
+    mapping(address => uint256) public totalShares;
+
+    // Define roles for access control
+    bytes32 public constant OWNER_ROLE = keccak256("OWNER_ROLE");
+
+    constructor() ERC20("FriendTech", "FTK") {
+        owner = msg.sender;
+
+        // Assign the contract owner the OWNER_ROLE
+        _setupRole(OWNER_ROLE, msg.sender);
+        _setRoleAdmin(OWNER_ROLE, OWNER_ROLE);
+    }
+
+    modifier onlyOwner() {
+        require(hasRole(OWNER_ROLE, msg.sender), "Caller is not the owner");
+        _;
+    }
+
+    function setSharePrice(uint256 price) external onlyOwner {
+        require(price > 0, "Price must be greater than zero");
+        sharePrice[msg.sender] = price;
+    }
+
+    function getSharePrice(address user) public view returns (uint256) {
+        return sharePrice[user];
+    }
+
+    function setTotalShares(uint256 amount) external {
+        require(amount > 0, "Amount must be greater than zero");
+        totalShares[msg.sender] = amount;
+    }
+
+    function getTotalShares(address user) public view returns (uint256) {
+        return totalShares[user];
+    }
+
+    function buyShares(address seller, uint256 amount) external payable {
+        require(amount > 0, "Amount must be greater than zero");
+        require(totalShares[seller] >= amount, "Seller does not have enough shares");
+        require(sharePrice[seller] <= msg.value, "Insufficient payment");
+
+        totalShares[seller] -= amount;
+        totalShares[msg.sender] += amount;
+
+        // Calculate the amount of tokens to mint based on the share price
+        uint256 tokensToMint = (msg.value * 10**decimals()) / sharePrice[seller];
+        _mint(msg.sender, tokensToMint);
+    }
+
+    function sellShares(address buyer, uint256 amount) external {
+        require(amount > 0, "Amount must be greater than zero");
+        require(totalShares[msg.sender] >= amount, "Insufficient shares");
+
+        totalShares[msg.sender] -= amount;
+        totalShares[buyer] += amount;
+
+        // Calculate the amount of tokens to burn based on the share price
+        uint256 tokensToBurn = (amount * sharePrice[msg.sender]) / 10**decimals();
+        _burn(msg.sender, tokensToBurn);
+        payable(buyer).transfer(tokensToBurn);
+    }
+
+    function transferShares(address to, uint256 amount) external {
+        require(amount > 0, "Amount must be greater than zero");
+        require(totalShares[msg.sender] >= amount, "Insufficient shares");
+
+        totalShares[msg.sender] -= amount;
+        totalShares[to] += amount;
+
+        // Transfer the corresponding amount of tokens
+        _transfer(msg.sender, to, amount);
+    }
+}
